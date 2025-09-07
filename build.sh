d80c27086ff3739d112e48a083a58af717721721
Marco <111095859+Specktic@users.noreply.github.com>
2025-09-07 15:21:41 -0600

    Escribir el dockerfile y build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Compiling"
+
+mkdir -p build
+
+riscv64-unknown-elf-gcc -Wall -O2 -nostartfiles \
+    -T linker.ld \
+    RISCV/startup.s \
+    C/main.c \
+    RISCV/tea_encrypt.asm \
+    RISCV/tea_decrypt.asm \
+    -o build/tea.elf
+
+echo "✅ Compilación completa: build/tea.elf"
